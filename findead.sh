80a53c7e9ad3d0890a45dd97a55977f4337fd9e6
MarcelloVSilva <marcellovcs@gmail.com>
2020-01-12 17:23:44 -0300

    started to get function components

--- findead.sh
+++ findead.sh
@@ -4,7 +4,7 @@ declare -a COMPONENTS
 AUX=''
 FOLDERS_TO_SEARCH_COMPONENTS=$1
 for FOLDER_TO_SEARCH_IMPORTS; do true; done
-LS=$(ls -R $FOLDERS_TO_SEARCH_COMPONENTS | awk '/:$/&&f{s=$0;f=0} /:$/&&!f{sub(/:$/,"");s=$0;f=1;next} NF&&f{ print s"/"$0 }')
+LS=$(find $FOLDERS_TO_SEARCH_COMPONENTS -type f \( -name "*.js" -o -name "*.jsx" \))
 QNTY_COMP_UNUSED=0
 AUX_QNTY=0
 
@@ -13,28 +13,40 @@ PID=$!
 i=1
 sp="/-\|"
 echo -n ' '
-while [ -d /proc/$PID ]
-do
+while [ -d /proc/$PID ]; do
   printf "\b${sp:i++%${#sp}:1}"
 done
 
+F_NAME=''
 getClassComponents() {
+  CLASS_COMPONENT=$(sudo cat ${F_NAME} | grep -oP '(?<=class).*?(Component)' | awk '{ print $1 }')
+  COMPONENTS+=($CLASS_COMPONENT)
+  AUX=${COMPONENTS[@]}
+}
+
+getFunctionComponents() {
+  IS_REACT_FILE=$(sudo cat ${F_NAME} | grep -oP '(?<=import).*?(React)')
+  if [ ! -z "$IS_REACT_FILE" ]; then
+    FUNCTIONS=$(sudo cat ${F_NAME} | grep -oP '(?<=function ).*?(?=\()')
+    for FUNCTION in $FUNCTIONS; do
+      FIRST_LETTER_FUNCTION_COMPONENT="$(echo "$FUNCTION" | head -c 1)"
+      [[ "$FIRST_LETTER_FUNCTION_COMPONENT" =~ [A-Z] ]] && COMPONENTS+=($FUNCTION)
+    done
+  fi
+  AUX=${COMPONENTS[@]}
+}
+
+getComponents() {
   for i in $LS; do
     F_NAME=$i
-    if test -f "${F_NAME}"
-    then
-      CLASS_COMPONENT=$(sudo cat ${F_NAME} | grep -oP '(?<=class).*?(Component)' | awk '{ print $1 }')
-      FIRST_LETTER_COMPONENT="$(echo $CLASS_COMPONENT | head -c 1)"
-      [[ "$FIRST_LETTER_COMPONENT" =~ [A-Z] ]] && COMPONENTS+=($CLASS_COMPONENT)
-    fi
-    AUX=${COMPONENTS[@]}
+    getClassComponents
+    getFunctionComponents
   done
 }
 
 searchImports() {
   GREP_RECURSIVE_RESULT=''
-  for i in ${COMPONENTS[@]}
-  do
+  for i in ${COMPONENTS[@]}; do
     GREP_RECURSIVE_RESULT=$(grep -r -oP '(?<=import).*?('$i')' $FOLDER_TO_SEARCH_IMPORTS)
     [[ -z "$GREP_RECURSIVE_RESULT" ]] && echo -e "\e[39m$i -> \e[33mUNUSED COMPONENT" && ((QNTY_COMP_UNUSED++))
     AUX_QNTY=$QNTY_COMP_UNUSED
@@ -42,12 +54,11 @@ searchImports() {
 }
 
 finalChapter() {
-  if [ $QNTY_COMP_UNUSED -eq 0 ]
-  then
+  if [ $QNTY_COMP_UNUSED -eq 0 ]; then
     echo -e "\e[32mYou don't have unused components :) \e[39m"
   else
-   echo -e "\e[33m$QNTY_COMP_UNUSED unused components :/ \e[39m"
-  fi 
+    echo -e "\e[33m$QNTY_COMP_UNUSED unused components :/ \e[39m"
+  fi
 }
 
-getClassComponents && searchImports && finalChapter
+getComponents && searchImports && finalChapter
