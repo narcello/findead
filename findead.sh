e08a0edaad141352800c633dea97ae37066d2433
MarcelloVSilva <marcellovcs@gmail.com>
2020-01-13 22:36:41 -0300

    refactor and enhance code

--- findead.sh
+++ findead.sh
@@ -1,31 +1,23 @@
 CLASS_COMPONENT=''
 FIRST_LETTER_COMPONENT=''
 declare -a COMPONENTS
-AUX=''
+AUX_ARRAY_COMPONENTS=''
 FOLDERS_TO_SEARCH_COMPONENTS=$1
 for FOLDER_TO_SEARCH_IMPORTS; do true; done
-LS=$(find $FOLDERS_TO_SEARCH_COMPONENTS -type f \( -name "*.js" -o -name "*.jsx" \))
-QNTY_COMP_UNUSED=0
-AUX_QNTY=0
+FIND_RETURN=$(find $FOLDERS_TO_SEARCH_COMPONENTS -type f \( -name "*.js" -o -name "*.jsx" \))
+COUNTER_UNUSED_COMPONENTS=0
+AUX_COUNTER=0
 
-sleep 60 &
-PID=$!
-i=1
-sp="/-\|"
-echo -n ' '
-while [ -d /proc/$PID ]; do
-  printf "\b${sp:i++%${#sp}:1}"
-  sleep 0.1s
-done
+echo 'Findead are looking for components...' && \
 
-F_NAME=''
+FILE_PATH=''
 getClassComponents() {
-  CLASS_COMPONENT=$(sudo cat ${F_NAME} | grep -oP '(?<=class).*?(Component)' | awk '{ print $1 }')
-  USED_IN_SAME_FILE=$(grep "<${CLASS_COMPONENT}" ${F_NAME})
+  CLASS_COMPONENT=$(cat ${FILE_PATH} | grep -oP '(?<=class).*?(Component)' | awk '{ print $1 }')
+  USED_IN_SAME_FILE=$(grep "<${CLASS_COMPONENT}" ${FILE_PATH})
   if [ ! -z "$CLASS_COMPONENT" ]; then
     [[ -z "$USED_IN_SAME_FILE" ]] && COMPONENTS+=($CLASS_COMPONENT)
   fi
-  AUX=${COMPONENTS[@]}
+  AUX_ARRAY_COMPONENTS=${COMPONENTS[@]}
 }
 
 CURRENT_FUNCTIONS=''
@@ -33,24 +25,24 @@ checkFunctions() {
   for FUNCTION in $CURRENT_FUNCTIONS; do
     FIRST_LETTER_FUNCTION_COMPONENT="$(echo "$FUNCTION" | head -c 1)"
     [[ "$FIRST_LETTER_FUNCTION_COMPONENT" =~ [A-Z] ]] &&
-      USED_IN_SAME_FILE=$(grep "<${FUNCTION}" ${F_NAME}) &&
+      USED_IN_SAME_FILE=$(grep "<${FUNCTION}" ${FILE_PATH}) &&
       [[ -z "$USED_IN_SAME_FILE" ]] && COMPONENTS+=($FUNCTION)
-    AUX=${COMPONENTS[@]}
+    AUX_ARRAY_COMPONENTS=${COMPONENTS[@]}
   done
 }
 
 getES5FunctionComponents() {
-  CURRENT_FUNCTIONS=$(sudo cat ${F_NAME} | grep -oP '(?<=function ).*?(?=\()')
+  CURRENT_FUNCTIONS=$(cat ${FILE_PATH} | grep -oP '(?<=function ).*?(?=\()')
   checkFunctions
 }
 
 getES6FunctionComponents() {
-  CURRENT_FUNCTIONS=$(sudo cat ${F_NAME} | grep -oP '(?<=const ).*?(?=\()' | awk '{ print $1 }')
+  CURRENT_FUNCTIONS=$(cat ${FILE_PATH} | grep -oP '(?<=const ).*?(?=\()' | awk '{ print $1 }')
   checkFunctions
 }
 
 getFunctionComponents() {
-  IS_REACT_FILE=$(sudo cat ${F_NAME} | grep -oP '(?<=import).*?(React)')
+  IS_REACT_FILE=$(cat ${FILE_PATH} | grep -oP '(?<=import).*?(React)')
   if [ ! -z "$IS_REACT_FILE" ]; then
     getES5FunctionComponents
     getES6FunctionComponents
@@ -58,8 +50,8 @@ getFunctionComponents() {
 }
 
 getComponents() {
-  for i in $LS; do
-    F_NAME=$i
+  for i in $FIND_RETURN; do
+    FILE_PATH=$i
     getClassComponents
     getFunctionComponents
   done
@@ -69,17 +61,17 @@ searchImports() {
   GREP_RECURSIVE_RESULT=''
   for i in ${COMPONENTS[@]}; do
     GREP_RECURSIVE_RESULT=$(grep -r -oP '(?<=import).*?('$i')' $FOLDER_TO_SEARCH_IMPORTS)
-    [[ -z "$GREP_RECURSIVE_RESULT" ]] && echo -e "\e[39m$i -> \e[33mUNUSED COMPONENT" && ((QNTY_COMP_UNUSED++))
-    AUX_QNTY=$QNTY_COMP_UNUSED
+    [[ -z "$GREP_RECURSIVE_RESULT" ]] && echo -e "\e[39m$i -> \e[33mUNUSED COMPONENT" && ((COUNTER_UNUSED_COMPONENTS++))
+    AUX_COUNTER=$COUNTER_UNUSED_COMPONENTS
   done
 }
 
-finalChapter() {
-  if [ $QNTY_COMP_UNUSED -eq 0 ]; then
+showResult() {
+  if [ $COUNTER_UNUSED_COMPONENTS -eq 0 ]; then
     echo -e "\e[32mYou don't have unused components :) \e[39m"
   else
-    echo -e "\e[33m$QNTY_COMP_UNUSED possible dead components :/ \e[39m"
+    echo -e "\e[33m$COUNTER_UNUSED_COMPONENTS possible dead components :/ \e[39m"
   fi
 }
 
-getComponents && searchImports && finalChapter
+getComponents && searchImports && showResult
