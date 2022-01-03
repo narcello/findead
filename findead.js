d29bee64224eb01e5f0b4e025c3f35a7f5ecaf34
narcello <marcellovcs@gmail.com>
2022-01-03 15:19:51 -0300

    feat: show unused files

--- findead.js
+++ findead.js
@@ -1,19 +1,14 @@
-// read the path
-// get all js, jsx, ts, tsx files in path
-// searchFiles
-
-// -> getComponents
-// searchImports
-
 const path = require("path");
 const fs = require("fs");
 const allFiles = [];
+const unusedFiles = [];
 
 const regexFilesTypes = /(?<!chunk|min|stories|test|bundle)\.(js|ts|jsx|tsx)/;
 const ignoredFolders = /(node_modules|dist|build|bin|out|output|target|log|logs|test|tests)/;
 
 getFiles(process.argv[2]);
-console.log(allFiles);
+searchImports(allFiles);
+console.log(unusedFiles);
 
 function getFiles(startPath) {
   if (!fs.existsSync(startPath)) {
@@ -31,4 +26,41 @@ function getFiles(startPath) {
       allFiles.push(filename);
     }
   }
+}
+
+function searchImports(filesNames) {
+  filesNames.forEach((fileName, index, array) => {
+    let baseName = path.basename(fileName);
+    baseName = baseName.replace(path.extname(fileName), "");
+
+    const es6ImportRegExp = constructEs6ImportRegExp(baseName);
+    const es5ImportRegExp = constructEs5ImportRegExp(baseName);
+    const lazyLoadImportRegExp = constructLazyLoadImportRegExp(baseName);
+
+    const isUsedFile = array.some((file) => {
+      try {
+        const data = fs.readFileSync(file, "utf8");
+        return (
+          es6ImportRegExp.test(data) ||
+          es5ImportRegExp.test(data) ||
+          lazyLoadImportRegExp.test(data)
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    });
+    if (!isUsedFile) unusedFiles.push(fileName);
+  });
+}
+
+function constructEs6ImportRegExp(baseName) {
+  return new RegExp(`import.*from (\\"|\\')(.*)${baseName}(\\"|\\')`, "gm");
+}
+
+function constructEs5ImportRegExp(baseName) {
+  return new RegExp(`require\\((\\"|\\')(.*)${baseName}(\\"|\\')\\)`, "gm");
+}
+
+function constructLazyLoadImportRegExp(baseName) {
+  return new RegExp(`import\\((\\"|\\')(.*)${baseName}(\\"|\\')\\)`, "gm");
 }
---
+++ src/imports/index2.js
@@ -0,0 +1,7 @@
+const routes = [
+  {
+    path: "/errors/error-500",
+    exact: true,
+    component: lazy(() => import("../src/components/N")),
+  },
+];
