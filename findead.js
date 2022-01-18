8bf91dea760528b0b37453f37ecd1197239f18be
narcello <marcellovcs@gmail.com>
2022-01-18 07:19:29 -0300

    chore: fix findead file based on eslint rules

--- findead.js
+++ findead.js
@@ -1,24 +1,47 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
+
 const allFiles = [];
 const unusedFiles = [];
 
 const regexFilesTypes = /(?<!chunk|min|stories|test|bundle|setup)\.(js|ts|jsx|tsx)/;
 const ignoredFolders = /(node_modules|dist|build|bin|out|output|coverage|target|log|logs|test|tests|.storybook)/;
 
-getFiles(process.argv[2]);
-searchImports(allFiles);
-console.log(unusedFiles);
+function constructEs6ImportRegExp(baseName) {
+  return new RegExp(`(.*)import.*from (\\"|\\')(.*)${baseName}(\\"|\\')`, 'g');
+}
+
+function constructEs5ImportRegExp(baseName) {
+  return new RegExp(`(.*)require\\((\\"|\\')(.*)${baseName}(\\"|\\')\\)`, 'g');
+}
+
+function constructLazyLoadImportRegExp(baseName) {
+  return new RegExp(`(.*)import\\((\\"|\\')(.*)${baseName}(\\"|\\')\\)`, 'g');
+}
+
+function singleLineCommentRegExp() {
+  return /\/\/.*/g;
+}
+
+function multiLineCommentRegExp() {
+  return /\/\*[\s\S]*\*\//g;
+}
+
+function removeComments(data) {
+  return data
+    .replace(multiLineCommentRegExp(), '')
+    .replace(singleLineCommentRegExp(), '');
+}
 
 function getFiles(startPath) {
   if (!fs.existsSync(startPath)) {
-    console.log("no dir ", startPath);
+    console.log('no dir ', startPath);
     return;
   }
 
   const files = fs.readdirSync(startPath);
 
-  for (var i = 0; i < files.length; i++) {
+  for (let i = 0; i < files.length; i += 1) {
     const filename = path.join(startPath, files[i]);
     const stat = fs.lstatSync(filename);
 
@@ -32,14 +55,14 @@ function getFiles(startPath) {
 
 function thereIsIndexFile(directoryPath) {
   const files = fs.readdirSync(directoryPath);
-  const hasIndexFile = files.some((file) => file.indexOf("index") > -1);
+  const hasIndexFile = files.some((file) => file.indexOf('index') > -1);
   return hasIndexFile;
 }
 
 function searchImports(filesPath) {
   filesPath.forEach((filePath, index, array) => {
     let baseName = path.basename(filePath);
-    baseName = baseName.replace(path.extname(filePath), "");
+    baseName = baseName.replace(path.extname(filePath), '');
 
     const fileFolder = path.dirname(filePath);
     const hasIndexFile = thereIsIndexFile(fileFolder);
@@ -51,45 +74,26 @@ function searchImports(filesPath) {
 
     const isUsedFile = array.some((file) => {
       try {
-        const data = fs.readFileSync(file, "utf8");
+        const data = fs.readFileSync(file, 'utf8');
 
         const dataWithoutComments = removeComments(data);
 
         return (
-          es6ImportRegExp.test(dataWithoutComments) ||
-          es5ImportRegExp.test(dataWithoutComments) ||
-          lazyLoadImportRegExp.test(dataWithoutComments)
+          es6ImportRegExp.test(dataWithoutComments)
+          || es5ImportRegExp.test(dataWithoutComments)
+          || lazyLoadImportRegExp.test(dataWithoutComments)
         );
       } catch (err) {
-        console.error(err);
+        return false;
       }
     });
+
     if (!isUsedFile) unusedFiles.push(filePath);
   });
 }
 
-function constructEs6ImportRegExp(baseName) {
-  return new RegExp(`(.*)import.*from (\\"|\\')(.*)${baseName}(\\"|\\')`, "g");
-}
-
-function constructEs5ImportRegExp(baseName) {
-  return new RegExp(`(.*)require\\((\\"|\\')(.*)${baseName}(\\"|\\')\\)`, "g");
-}
-
-function constructLazyLoadImportRegExp(baseName) {
-  return new RegExp(`(.*)import\\((\\"|\\')(.*)${baseName}(\\"|\\')\\)`, "g");
-}
-
-function singleLineCommentRegExp() {
-  return new RegExp("//.*", "g");
-}
-
-function multiLineCommentRegExp() {
-  return new RegExp("\\/\\*[\\s\\S]*\\*\\/", "g");
-}
-
-function removeComments(data) {
-  return data
-    .replace(multiLineCommentRegExp(), "")
-    .replace(singleLineCommentRegExp(), "");
-}
+console.time();
+getFiles(process.argv[2]);
+searchImports(allFiles);
+console.log(unusedFiles);
+console.timeEnd();
