b498c1c9dfe950964419f96549a423fb03cacd36
narcello <marcellovcs@gmail.com>
2022-01-18 07:18:48 -0300

    chore: eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true,
   },
-  extends: ["airbnb-base"],
+  extends: [
+    'airbnb-base',
+  ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
   },
   rules: {
-    quotes: ["error", "double"],
+    'no-console': 0
   },
 };
