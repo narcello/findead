bc42325d3aca078d22d4b59b04e750146c481e0e
narcello <marcellovcs@gmail.com>
2022-01-21 07:32:09 -0300

    test: fighting with async tests

--- findead.test.ts
+++ findead.test.ts
@@ -1,11 +1,14 @@
-import fs from 'fs'
 import { exec } from 'child_process'
+jest.useFakeTimers('legacy')
 
 describe('Findead', () => {
-  it('first test', async () => {
-    const script = 'ts-node ./findead.ts src'
-    await exec(script, () => {
-      expect(1+1).toEqual(2)
-    })
+  it('first test', (done) => {
+    const script = 'ts-node ./findead.ts src/components'
+    exec(script, (error, stdout, stderr) => {
+      console.log(stdout);
+      if(error || stderr) done();
+      expect(stdout.indexOf('src/imports/inde2.js')).toBeGreaterThan(-1)
+      done()
+    });
   })
 })
